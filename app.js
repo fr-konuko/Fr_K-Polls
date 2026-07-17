bc56e617ca88dfcc4d07107d5678be8b5a28d5b9
fr-konuko <fr.ochieng01@gmail.com>
2026-07-17 15:49:14 +0300

    Update ADMIN_PASSCODE from 'FRK2002' to 'FRK2026'

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 (function(){
- const ADMIN_PASSCODE = 'FRK2002';
+ const ADMIN_PASSCODE = 'FRK2026';
   const POSITIONS = ['President','Governor','Senetor','Women Rep','Member of Parliament','MCA'];
   const PLACEHOLDER = 'https://placehold.co/500x500/eaf5ff/0f4c81?text=Photo';
   const COLORS = ['#0f4c81','#0ea5e9','#f59e0b','#059669','#7c3aed','#ef4444','#14b8a6','#334155'];
